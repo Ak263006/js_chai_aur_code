6e5b36ff75c2824ffb492365b18dfd2c3ea7b55c
Ak263006 <aashish.25b01011380@abes.ac.in>
2026-08-17 12:58:23 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,34 @@
+"use strict"; //trat all js code as newer version
+
+//alert(3+3) // we are using node.js not browser.
+
+console.log(3 + 3); console.log("hitesh")//code readability decreases so avoid using them as such.
+//code readeability should be high.
+
+
+let name = "aashish"
+let age = 20
+let isloggedIn= false
+let state = null
+
+//primitive data types:
+//number =>2 to power 53
+//bigint for very large numbers
+
+//string =>can use in both single and double quotes.prefer double
+
+//boolean => true/false
+
+//null => it is a standalone value.
+
+//undefined => variable declared but no value is given.
+
+//symbol => used during react (unique)
+
+
+//OBJECT:
+
+
+console.log(typeof null)//object
+console.log(typeof undefined)//undefined
+
