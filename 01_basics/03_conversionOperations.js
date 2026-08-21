0f186ea98c7e7d23c6d9644a71c4162c8da4e1fd
Ak263006 <aashish.25b01011380@abes.ac.in>
2026-08-21 05:43:30 +0000

    conversion of dataTypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,29 @@
+// let score= '33'
+
+
+// console.log(typeof score)
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log( valueInNumber)
+
+//"33"=>33
+//"33abc"=> NaN(type will show number)
+//null=>true->1,false->0
+//undefined=>NaN
+//string=>NaN
+
+// let isLoggedIn = undefined
+// let booleanIsLoggedIn= Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+//1=>true  0=>false
+//emptyString => false nonEmptyString =>true
+//null and undefined=>false
+
+
+let someNumber = 34
+console.log(typeof someNumber);
+let stringSomeNumber = String(someNumber)
+
+console.log( typeof stringSomeNumber);
