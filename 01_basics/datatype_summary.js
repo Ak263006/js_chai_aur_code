a3ae2e9d7a9ebf2ad3427781a31cde86c7bd4d45
Ak263006 <aashish.25b01011380@abes.ac.in>
2026-08-22 05:11:47 +0000

    datatypensummary

---
+++ 01_basics/datatype_summary.js
@@ -0,0 +1,33 @@
+//datatype is categorized into two types based on their memoey allocation.
+//javascript is dynamically typed language.
+
+const score = 100;
+const scoreValue = 100-.443
+const id = Symbol('123')
+const anotherId= Symbol('123')
+
+// console.log(id==anotherId)
+
+//primitive(called by value)
+//  7  Types: string, Number, Boolean, null, undefined, Symbol(to make a value unique), BigInt(to handle very large values).
+
+//Non-Primitive(reference type)
+// Array , Objects, Functions
+
+const heroes =["shaktiman", "makkadmanav","halku"]
+let myObj = {
+    name:"aashish",
+    age:20,
+}
+
+const myFunction = function(){
+    console.log("Hello World!")
+}
+
+console.log(typeof myFunction)//function
+console.log(typeof myObj)//object
+console.log (typeof heroes)//object
+console.log(typeof null)//object
+console.log(typeof undefined)//undefined
+console.log (typeof "hitesh")//string
+
